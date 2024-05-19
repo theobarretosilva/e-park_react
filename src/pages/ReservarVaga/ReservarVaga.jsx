343d8fa937a5f00ac7ede0a9e48eb88cc073e083
Théo Barreto Silva <barretotheo25@gmail.com>
2024-05-19 18:34:06 -0300

    pagina de reservar vaga pronta

--- src/pages/ReservarVaga/ReservarVaga.jsx
+++ src/pages/ReservarVaga/ReservarVaga.jsx
@@ -1,7 +1,149 @@
+import { useNavigate } from 'react-router';
+import * as S from './ReservarVaga.styles'
+import { useState } from 'react';
+
 export function ReservarVaga() {
-    return(
-        <>
-            TELA PARA RESERVAR VAGA
-        </>
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        vehiclePlate: '',
+        ownerName: '',
+        apartmentNumber: '',
+        apartmentBlock: '',
+        vehicleModel: '',
+        vehicleColor: '',
+        parkingSpot: ''
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData(prevData => ({ ...prevData, [name]: value }));
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        let storedData = localStorage.getItem("Vagas reservadas");
+        let vagasReservadas;
+        try {
+            vagasReservadas = storedData ? JSON.parse(storedData) : [];
+            if (!Array.isArray(vagasReservadas)) {
+                throw new Error("O dado não é um Array");
+            }
+        } catch (error) {
+            vagasReservadas = [];
+        }
+
+        const vagaExiste = vagasReservadas.some(vaga => vaga.parkingSpot === formData.parkingSpot);
+
+        if (vagaExiste) {
+            alert("Esta vaga já está reservada. Por favor, selecione outra vaga.");
+        } else {
+            vagasReservadas.push(formData);
+
+            localStorage.setItem("Vagas reservadas", JSON.stringify(vagasReservadas));
+
+            alert("Vaga reservada com sucesso");
+            setFormData({
+                vehiclePlate: '',
+                ownerName: '',
+                apartmentNumber: '',
+                apartmentBlock: '',
+                vehicleModel: '',
+                vehicleColor: '',
+                parkingSpot: ''
+            });
+
+            navigate('listagemVagas/vagasReservadas')
+        }
+    }
+
+    return (
+        <S.MainStyled>
+            <S.ImgVoltar src="../../src/assets/svg/arrow-left.svg" onClick={() => navigate('/')}/>
+            <S.ImgLogo src='../../src/assets/svg/logo.svg' />
+            <S.DivForm>
+                <S.TituloForm>Reservar vaga</S.TituloForm>
+                <S.FormSyled onSubmit={handleSubmit}>
+                    <div>
+                        <S.TituloInput>Placa do veículo</S.TituloInput>
+                        <S.InputReserva
+                            type="text"
+                            name="vehiclePlate"
+                            value={formData.vehiclePlate}
+                            onChange={handleChange}
+                            required
+                            minLength="7"
+                            maxLength="8"
+                            pattern='[A-z]{3}-\d[A-j0-9]\d{2}'
+                        />
+                    </div>
+                    <S.DivInputs>
+                        <S.TituloInput>Nome do proprietário</S.TituloInput>
+                        <S.InputReserva
+                            type="text"
+                            name="ownerName"
+                            value={formData.ownerName}
+                            onChange={handleChange}
+                            required
+                            minLength="3"
+                        />
+                    </S.DivInputs>
+                    <S.DivInputs>
+                        <S.TituloInput>N° do apartamento</S.TituloInput>
+                        <S.InputReserva
+                            type="number"
+                            name="apartmentNumber"
+                            value={formData.apartmentNumber}
+                            onChange={handleChange}
+                            required
+                            min="1"
+                        />
+                    </S.DivInputs>
+                    <S.DivInputs>
+                        <S.TituloInput>Bloco do apartamento</S.TituloInput>
+                        <S.InputReserva
+                            type="text"
+                            name="apartmentBlock"
+                            value={formData.apartmentBlock}
+                            onChange={handleChange}
+                            required
+                        />
+                    </S.DivInputs>
+                    <S.DivInputs>
+                        <S.TituloInput>Modelo do veículo</S.TituloInput>
+                        <S.InputReserva
+                            type="text"
+                            name="vehicleModel"
+                            value={formData.vehicleModel}
+                            onChange={handleChange}
+                            required
+                        />
+                    </S.DivInputs>
+                    <S.DivInputs>
+                        <S.TituloInput>Cor do veículo</S.TituloInput>
+                        <S.InputReserva
+                            type="text"
+                            name="vehicleColor"
+                            value={formData.vehicleColor}
+                            onChange={handleChange}
+                            required
+                        />
+                    </S.DivInputs>
+                    <S.DivInputs>
+                        <S.TituloInput>N° da vaga de estacionamento</S.TituloInput>
+                        <S.InputReserva
+                            type="number"
+                            name="parkingSpot"
+                            value={formData.parkingSpot}
+                            onChange={handleChange}
+                            required
+                            min="1"
+                        />
+                    </S.DivInputs>
+                    <S.BtnReservar type="submit">Reservar vaga</S.BtnReservar>
+                </S.FormSyled>
+            </S.DivForm>
+        </S.MainStyled>
     )
 }
---
+++ src/pages/ReservarVaga/ReservarVaga.styles.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const MainStyled = styled.main`
+    background-color: #D3D3D3;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+`
+
+export const ImgVoltar = styled.img`
+    position: fixed;
+    top: 4vh;
+    left: 2.5vw;
+    cursor: pointer;
+`
+
+export const ImgLogo = styled.img`
+    width: 10vw;
+    height: 17vh;
+    object-fit: cover;
+    margin-bottom: 0;
+`
+
+export const DivForm = styled.div`
+    background-color: white;
+    width: 29vw;
+    height: 80vh;
+    border: 3px solid black;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 0;
+`
+
+export const TituloForm = styled.h1`
+    font-family: 'Inter', sans-serif;
+    font-size: 27px;
+    margin-top: 2vh;
+`
+
+export const FormSyled = styled.form`
+    width: 27.5vw;
+    height: 58vh;
+`
+
+export const DivInputs = styled.div`
+    margin-top: 1vh;
+`
+
+export const TituloInput = styled.h2`
+    font-family: 'Inter', sans-serif;
+    font-size: 17px;
+    font-weight: 600;
+    margin-bottom: 0;
+    margin-top: 0;
+`
+
+export const InputReserva = styled.input`
+    width: 97%;
+    height: 34px;
+    background-color: #F5F5F5;
+    border: 1px solid black;
+    border-radius: 7px;
+    font-family: 'Inter', sans-serif;
+    font-size: 15px;
+    padding-left: 2%;
+`
+
+export const BtnReservar = styled.button`
+    font-family: 'Inter', sans-serif;
+    font-size: 19px;
+    font-weight: 600;
+    color: white;
+    width: 100%;
+    height: 32px;
+    background-color: #A17222;
+    margin-top: 3vh;
+    border: 1px solid black;
+    border-radius: 7px;
+    cursor: pointer;
+`
